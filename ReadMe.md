4eb9cd50a0e3e4e570e0c378c518bd402c18040d
Leopold <leopolddag@gmail.com>
2023-04-20 14:39:44 +0200

    change for forEach for the loop

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,2 @@
 # Print-It WebSite v.1
+lien github page: https://alwesste.github.io/Projet-Print-it/
--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
+const banner = document.querySelector(".banner-img");
+const tagLine = document.querySelector(".tag");
+const dots = document.querySelectorAll('.dot');
+
 const slides = [
 	{
 		"image":"slide1.jpg",
@@ -17,80 +21,119 @@ const slides = [
 	}
 ]
 
-for (let i = 1; i < slides.length; i++ ) {
-    const banner = document.getElementById("banner");
-
-    const otherBanner = document.createElement("img");
-    otherBanner.src = "assets/images/slideshow/" + slides[i].image;
-    otherBanner.classList.add("banner-img");
-	otherBanner.style.display = "none";
-	banner.appendChild(otherBanner);
-	
-	const nouveauTag = document.createElement("p");
-	nouveauTag.classList.add("tag")	;
-	nouveauTag.innerHTML = slides[i].tagLine;
-	nouveauTag.style.display = "none";
-	banner.appendChild(nouveauTag);
-}
-
 
 let i = 0;
 let numSlides = 4;
-console.log("debut de right " + i)
-let arrow_right = document.querySelector(".arrow_right");
-arrow_right.addEventListener("click", toRight); 
 
-console.log("debut de left " + i)
-let arrow_left = document.querySelector(".arrow_left");
-arrow_left.addEventListener("click", toLeft); 
+const arrow_right = document.querySelector(".arrow_right");
+arrow_right.addEventListener("click", toRight);
+
+const arrow_left = document.querySelector(".arrow_left");
+arrow_left.addEventListener("click", toLeft)
 
 
 function toRight() {
-
-document
-	.querySelector(".dot_selected")
-	.classList.remove("dot_selected");
-document
-	.getElementsByClassName("banner-img")[i]
-	.style.display = "none";
-document
-	.getElementsByClassName("tag")[i]
-	.style.display = "none";
+	// if (i >= 2) {
+	// 	i = 0
+	// }else {
+	// 	i++
+	// }	
 
 	i = (i + 1) % numSlides;
+	dots.forEach(item => item.classList.remove("dot_selected"))
+	banner.src = "assets/images/slideshow/" + slides[i].image;
+	tagLine.innerHTML = slides[i].tagLine;
+	dots[i].classList.add("dot_selected")
+};
 
-document
-	.getElementsByClassName("dot")[i]
-	.classList.add("dot_selected");
-document
-	.getElementsByClassName("banner-img")[i]
-	.style.display = "block";
-document
-	.getElementsByClassName("tag")[i]
-	.style.display = 'block';
-}
 
 function toLeft() {
-
-document
-	.querySelector(".dot_selected")
-	.classList.remove("dot_selected");
-document
-	.getElementsByClassName("banner-img")[i]
-	.style.display = "none";
-document
-	.getElementsByClassName("tag")[i]
-	.style.display = "none";
-
-i = (i - 1 + numSlides) % numSlides;
-
-document
-	.getElementsByClassName("dot")[i]
-	.classList.add("dot_selected");
-document
-	.getElementsByClassName("banner-img")[i]
-	.style.display = "block";
-document
-	.getElementsByClassName("tag")[i]
-	.style.display = 'block';
+	dots.forEach(item => item.classList.remove("dot_selected"))
+	i = (i - 1 + numSlides) % numSlides;
+	banner.src = "assets/images/slideshow/" + slides[i].image;
+	tagLine.innerHTML = slides[i].tagLine
+	dots[i].classList.add("dot_selected")
 }
+
+
+
+// for (let i = 1; i < slides.length; i++ ) {
+//     const banner = document.getElementById("banner");
+
+//     const otherBanner = document.createElement("img");
+//     otherBanner.src = "assets/images/slideshow/" + slides[i].image; //slide.image
+//     otherBanner.classList.add("banner-img");
+// 	otherBanner.style.display = "none";
+// 	banner.appendChild(otherBanner);
+	
+// 	const nouveauTag = document.createElement("p");
+// 	nouveauTag.classList.add("tag")	;
+// 	nouveauTag.innerHTML = slides[i].tagLine;
+// 	nouveauTag.style.display = "none";
+// 	banner.appendChild(nouveauTag);
+// }
+
+// // slides.forEach(slide) => {
+	
+// // }
+
+// let i = 0;
+// let numSlides = 4;
+// console.log("debut de right " + i)
+// let arrow_right = document.querySelector(".arrow_right");
+// arrow_right.addEventListener("click", toRight); 
+
+// console.log("debut de left " + i)
+// let arrow_left = document.querySelector(".arrow_left");
+// arrow_left.addEventListener("click", toLeft); 
+
+
+// function toRight() {
+
+// document
+// 	.querySelector(".dot_selected")
+// 	.classList.remove("dot_selected");
+// document
+// 	.getElementsByClassName("banner-img")[i]
+// 	.style.display = "none";
+// document
+// 	.getElementsByClassName("tag")[i]
+// 	.style.display = "none";
+
+// i = (i + 1) % numSlides;
+
+// document
+// 	.getElementsByClassName("dot")[i]
+// 	.classList.add("dot_selected");
+// document
+// 	.getElementsByClassName("banner-img")[i]
+// 	.style.display = "block";
+// document
+// 	.getElementsByClassName("tag")[i]
+// 	.style.display = 'block';
+// }
+
+// function toLeft() {
+
+// document
+// 	.querySelector(".dot_selected")
+// 	.classList.remove("dot_selected");
+// document
+// 	.getElementsByClassName("banner-img")[i]
+// 	.style.display = "none";
+// document
+// 	.getElementsByClassName("tag")[i]
+// 	.style.display = "none";
+
+// i = (i - 1 + numSlides) % numSlides;
+
+// document
+// 	.getElementsByClassName("dot")[i]
+// 	.classList.add("dot_selected");
+// document
+// 	.getElementsByClassName("banner-img")[i]
+// 	.style.display = "block";
+// document
+// 	.getElementsByClassName("tag")[i]
+// 	.style.display = 'block';
+// }
